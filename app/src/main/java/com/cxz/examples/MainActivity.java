5a59374248d7ca04056809ef156b2e2682ac585a
chenxz <chenxiaozhen@ieforever.com>
2017-11-06 21:58:56 +0800

    Initial commit

---
+++ app/src/main/java/com/cxz/examples/MainActivity.java
@@ -0,0 +1,35 @@
+package com.cxz.examples;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.btn_shadow)
+    Button btn_shadow;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
+    }
+
+    @OnClick({R.id.btn_shadow})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_shadow:
+                break;
+        }
+
+    }
+
+}
