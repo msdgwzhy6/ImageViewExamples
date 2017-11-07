7b7c877973aa2de4de409d21cadeee0ac73fb1aa
chenxz <chenxiaozhen@ieforever.com>
2017-11-07 22:03:21 +0800

    add ShadowImageView

--- app/src/main/java/com/cxz/examples/MainActivity.java
+++ app/src/main/java/com/cxz/examples/MainActivity.java
@@ -1,5 +1,6 @@
 package com.cxz.examples;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -27,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.btn_shadow:
+                startActivity(new Intent(this,ShadowImageViewActivity.class));
                 break;
         }
 
